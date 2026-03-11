3d2fd12df599cd3ce0f9996b7e9b9f458fa578e8
qabelov1 <csgobelovv11@gmail.com>
2026-03-11 15:48:38 +0300

    Фикстуры Pytest

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,20 +1,21 @@
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import sync_playwright, expect, Page
 
-def test_wrong_email_or_password_auth ():
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        page = browser.new_page()
+from playwright_disabled import playwright
 
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
 
-        email_input = page.get_by_test_id('login-form-email-input').locator('input')
+def test_wrong_email_or_password_auth (chromium_page: Page):
+        chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+
+        email_input = chromium_page.get_by_test_id('login-form-email-input').locator('input')
         email_input.fill('user.name@gmail.com')
-        password_input = page.get_by_test_id('login-form-password-input').locator('input')
+
+        password_input = chromium_page.get_by_test_id('login-form-password-input').locator('input')
         password_input.fill('password')
-        button_login = page.get_by_test_id('login-page-login-button')
+
+        button_login = chromium_page.get_by_test_id('login-page-login-button')
         button_login.click()
 
-        wrong_email_or_password_alert = page.get_by_test_id('login-page-wrong-email-or-password-alert')
+        wrong_email_or_password_alert = chromium_page.get_by_test_id('login-page-wrong-email-or-password-alert')
         expect(wrong_email_or_password_alert).to_be_visible()
         expect(wrong_email_or_password_alert).to_have_text('Wrong email or password')
 
